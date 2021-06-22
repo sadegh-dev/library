0ef789bce68bd722015352677fab4005d0098467
charlie <sadegh.rez@gmail.com>
2021-06-22 16:58:29 +0430

    create accounts and transaction app

--- lib/library/models.py
+++ lib/library/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField()
+
+
+class Author(models.Model):
+    name = models.CharField()
+    url_description = models.URLField()
+
+
+class Translator(models.Model):
+    name = models.CharField()
+    url_description = models.URLField()
+
+
+class Publisher(models.Model):
+    name = models.CharField()
+    url_description = models.URLField()
+
+# class suitable_age in user model
+# class statuc
+
+class Book(models.Model):
+    #user =          models.ForeignKey()
+    #category =      models.ForeignKey()
+    #author =        models.ForeignKey()
+    #translator =    models.ForeignKey()
+    #suitable_age =  models.ForeignKey()
+    title =         models.CharField()
+    page_number =   models.PositiveIntegerField()
+    isbn =          models.PositiveBigIntegerField()
+    pic =           models.ImageField()
+    description =   models.TextField()
+    created =       models.DateTimeField()
+    updated =       models.DateTimeField()
+    
